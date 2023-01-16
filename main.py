24b5f07e8efc8e0d6e88255897753bf0bb05e234
Maxim-Pekov <max.pekov@gmail.com>
2023-01-16 18:04:40 +0300

    Change star logic to async code

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
+import asyncio
 import time
 import curses
 
 
-def draw(canvas):
+async def blink(canvas, row, column, symbol='*'):
     while True:
-        row, column = (5, 20)
-        curses.curs_set(False)
-        canvas.border()
+        canvas.addstr(row, column, symbol, curses.A_DIM)
+        await asyncio.sleep(0)
 
-        canvas.addstr(row, column, '*', curses.A_DIM)
-        time.sleep(2)
-        canvas.refresh()
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
 
-        canvas.addstr(row, column, '*')
-        time.sleep(0.3)
-        canvas.refresh()
+        canvas.addstr(row, column, symbol, curses.A_BOLD)
+        await asyncio.sleep(0)
 
-        canvas.addstr(row, column, '*', curses.A_BOLD)
-        time.sleep(0.5)
-        canvas.refresh()
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
 
-        canvas.addstr(row, column, '*')
-        time.sleep(0.3)
+
+def draw(canvas):
+    row, column = (5, 20)
+    star = blink(canvas, row, column, symbol='*')
+    curses.curs_set(False)
+    canvas.border()
+    while True:
+        star.send(None)
         canvas.refresh()
+    time.sleep(3)
 
 
 if __name__ == '__main__':
