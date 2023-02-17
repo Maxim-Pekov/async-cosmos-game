07a4a014ba00f55079c600d02ebfeb4db3fcea93
Maxim-Pekov <max.pekov@gmail.com>
2023-02-17 23:46:18 +0300

    Change rocket position.

--- main.py
+++ main.py
@@ -17,19 +17,19 @@ async def blink(canvas, row, column, symbol='*'):
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
         await asyncio.sleep(0)
-        time.sleep(0.02)
+        time.sleep(0.002)
 
         canvas.addstr(row, column, symbol)
         await asyncio.sleep(0)
-        time.sleep(0.003)
+        time.sleep(0.0003)
 
         canvas.addstr(row, column, symbol, curses.A_BOLD)
         await asyncio.sleep(0)
-        time.sleep(0.005)
+        time.sleep(0.0005)
 
         canvas.addstr(row, column, symbol)
         await asyncio.sleep(0)
-        time.sleep(0.0001)
+        time.sleep(0.0003)
 
 
 def draw_stars(canvas, window_height, window_width):
@@ -44,34 +44,51 @@ def draw_stars(canvas, window_height, window_width):
     return set_of_stars
 
 
-async def animate_spaceship(canvas, window_height, window_width):
+async def animate_spaceship(canvas, row, col, window_height, window_width):
+
     while True:
+        rows_direction, columns_direction, space_pressed = curses_tools.read_controls(
+            canvas)
+        row += rows_direction
+        col += columns_direction
+
+        if col <= -1:
+            col = -1
+        elif col >= window_width - 4:
+            col = window_width - 4
+        if row <= 0:
+            row = 0
+        elif row >= window_height - 2:
+            row = window_height - 2
+
         curses_tools.draw_frame(
-            canvas, window_height - 11, (window_width // 2) - 2, frame1
+            canvas, row, col, frame1
         )
-        time.sleep(0.3)
         await asyncio.sleep(0)
         curses_tools.draw_frame(
-            canvas, window_height - 11,
-            (window_width // 2) - 2,
+            canvas, row,
+            col,
             frame1, negative=True
         )
+
         curses_tools.draw_frame(
-            canvas, window_height - 11, (window_width // 2)- 2, frame2
+            canvas, row, col, frame2
         )
-        time.sleep(0.3)
         await asyncio.sleep(0)
         curses_tools.draw_frame(
-            canvas, window_height - 11, (window_width // 2) - 2,
+            canvas, row, col,
             frame2, negative=True
         )
 
 
 def draw(canvas):
     window_height, window_width = (curses.LINES, curses.COLS)
+    row = window_height - 11
+    col = (window_width // 2) - 2
+
     coroutines = draw_stars(canvas, window_height, window_width)
     coroutines.append(fire_animation.fire(canvas, start_row=window_height - 2, start_column=window_width // 2))
-    coroutines.append(animate_spaceship(canvas, window_height, window_width))
+    coroutines.append(animate_spaceship(canvas, row, col, window_height, window_width))
 
 
     while True:
@@ -82,7 +99,9 @@ def draw(canvas):
                 coroutines.remove(coroutine)
             curses.curs_set(False)
             canvas.border()
+            canvas.nodelay(True)
             canvas.refresh()
+            time.sleep(0.001)
         if len(coroutines) == 0:
             break
 
